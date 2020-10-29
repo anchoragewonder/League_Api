b53accb0f3c2ba551639d7665b3921e6e3212ea6
anchoragewonder <anchoragewonder@gmail.com>
2020-10-29 17:24:29 -0400

    Initial get statement

---
+++ League_Api/DbSchema/TableInterface.cs
@@ -0,0 +1,42 @@
+﻿using League_Api.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using MySql;
+
+
+namespace League_Api.DbSchema
+{
+    class TableInterface
+    {
+        // formating is going to be wrong but general concept
+        public void GetChamp()
+        {
+
+            try
+            {
+                
+                using var Connection = new MySqlConnection();//(connection_string);
+                Connection.Open();
+                MySqlCommand Cmd = Connection.CreateCommand();
+                Cmd.CommandText = "SELECT * FROM leagueOfLegendsChampionData WHERE Name = Aatrox ";
+                MySqlDataReader sqlRead = Cmd.ExecuteReader();
+
+                while (sqlRead.Read())
+                {
+                    Console.WriteLine(sqlRead);
+                }
+
+                sqlRead.Close();
+                Connection.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+           
+        }
+    }
+}
