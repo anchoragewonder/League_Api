c379f10681ae1fa40236383543fc38168dc4c836
anchoragewonder <anchoragewonder@gmail.com>
2020-10-30 23:56:00 -0400

    define champ Model

---
+++ League_Api/ResponseModels/GetChampionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace League_Api.ResponseModels
+{
+    class GetChampionModel
+    {
+
+        [JsonProperty(PropertyName = "name", Order = 1)]
+        public string Name { get; set; }
+    }
+}
---
+++ League_Api/TableModels/ChampModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace League_Api.TableModels
+{
+    public class ChampModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public int Style { get; set; }
+        public int Difficulty { get; set; }
+        public string DamageType { get; set; }
+        public int Damage { get; set; }
+        public int Sturdiness { get; set; }
+        public int CrowdControl { get; set; }
+        public int Mobility { get; set; }
+        public int Functionality { get; set; }
+
+        public ChampModel() { }
+
+        public ChampModel(int id, string name, string _class, int style, int difficulty, 
+            string damageType, int damage, int sturdiness, int crowdControl, int mobility, 
+            int functionality)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Class = _class;
+            this.Style = style;
+            this.Difficulty = difficulty;
+            this.DamageType = damageType;
+            this.Damage = damage;
+            this.Sturdiness = sturdiness;
+            this.CrowdControl = crowdControl;
+            this.Mobility = mobility;
+            this.Functionality = functionality;
+        }
+    }
+}
